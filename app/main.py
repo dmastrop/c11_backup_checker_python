a4373a1f6ce37374fbf50e2ba8d7d2726eebe82b
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2024-10-15 19:15:22 +0000

    test

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ from tabulate import tabulate
 # test5
 # test6
 # test7
+# test8
 # This will load env vars from the .env file
 # They will be available to use in the rest of the code blocks below
 load_dotenv()
